b754460e8fc38991eb3052e7ce287411a3c78bf2
VJacobo <victoriajacobo99@gmail.com>
2023-11-02 16:06:45 -0500

    added a db.js file. Now has the code in order to get and callback info from mysql

---
+++ db.js
@@ -0,0 +1,21 @@
+// setting up the variable to use mysql.
+// setting up for the database to grab info from the lib/suggestions file.
+const mysql = require("mysql");
+const prompts = require("./lib/prompts");
+const { connect } = require("http2");
+
+// creating the mysql connection to localhost/3305.
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3305,
+    user: root,
+    password: 123456,
+    database: "employee_tracker_module_12"
+});
+
+// letting us know wheather or not there has been a connection with a callback function.
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("connected as id " + connection.threadId + "/n");
+    prompts.connection = connect
+});
